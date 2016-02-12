a653427e420418f67fcf9f4417962c13314a6e3b
jackyef <jacky13ti@mahasiswa.pcr.ac.id>
2016-02-12 18:50:02 +0700

    Update connect.php

--- YourVote_services/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$mysqli = new mysqli("mysql.idhostinger.com", "u116662735_root", "root123", 'u116662735_yvote');
-
-?>
--- connect.php
+++ connect.php
@@ -1,4 +1,6 @@
 <?php
-$mysqli = new mysqli("mysql.idhostinger.com", "u116662735_root", "root123", 'u116662735_yvote');
+//$mysqli = new mysqli("mysql.idhostinger.com", "u116662735_root", "root123", 'u116662735_yvote');
+$mysqli = new mysqli("ap-cdbr-azure-southeast-b.cloudapp.net", "b775a01494ccad", "2c3ebeb7", 'YourVote_db');
+
 
 ?>
